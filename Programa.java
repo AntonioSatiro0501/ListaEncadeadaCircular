eff182fa2322edb6567fed39895558bd10063126
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2025-10-28 13:24:29 +0000

    Leitor de arquivos java

---
+++ Programa.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class Programa {
+    public static void main(String[] args) {
+        String[] linhas = null;
+        try{
+
+            linhas = lerArquivo("arquivo.txt");
+            for(int i = 0; i < linhas.length; i++){
+                if (linhas[i] == null) break;
+                System.out.println(linhas[i]);
+            }
+        } 
+        catch(IOException e) {
+            System.out.printf("Erro ao abrir arquivo");
+        }
+
+        if (linhas != null){
+            linhas[9] = "100 out a";
+            linhas[10] = "110 out b";
+            try{
+                gravaArquivo("arquivo.txt", linhas);
+            }
+            catch (IOException e){
+                System.out.printf("Erro ao gravar arquivo: %s\n", e.getMessage());
+            }
+        }
+    }
+
+    public static String[] lerArquivo(String caminho) throws IOException {
+
+        String linha;
+        String[] linhas = new String[100];
+
+        File arquivo = new File(caminho);
+        Scanner leitor = new Scanner(arquivo);
+        int posicao = 0;
+        while(leitor.hasNextLine()){
+            linha = leitor.nextLine();
+            linhas[posicao] = linha;
+            posicao++;
+        }
+
+        leitor.close();
+        //https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
+
+        return linhas;
+    }
+
+    public static void gravaArquivo(String caminho, String[] linhas) throws IOException{
+        PrintWriter escritor = new PrintWriter(caminho);
+        int posicao = 0;
+        while(linhas[posicao] != null){
+
+            escritor.println(linhas[posicao]);
+            posicao++;
+        }
+
+        escritor.close();
+    }
+}
